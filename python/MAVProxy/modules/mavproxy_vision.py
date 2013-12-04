f928571272279c167c6f7bc39927ec1fabdb08eb
Henry Bradlow <hbradlow@berkeley.edu>
2013-12-04 12:49:17 -0800

    Fixed mavproxy stuff

--- python/MAVProxy/modules/mavproxy_vision.py
+++ python/MAVProxy/modules/mavproxy_vision.py
@@ -2,35 +2,44 @@ import threading
 from pykalman import KalmanFilter
 import time
 import libmain_py as vision
+from numpy import ma
 
 mpstate = None
 
 class Tracker:
     def __init__(self,alpha=.5):
-	self.mean = [0,0,0]
-	self.cov = None
-	self.kf = KalmanFilter(initial_state_mean=self.mean,n_dim_obs=3)
+        self.mean = [0,0,0]
+        self.cov = None
+        self.kf = KalmanFilter(initial_state_mean=self.mean,n_dim_obs=3)
 
-	means,c = self.kf.filter([self.mean,self.mean])
-	self.mean = means[-1]
-	self.cov = c[-1]
+        means,c = self.kf.filter([self.mean,self.mean])
+        self.mean = means[-1]
+        self.cov = c[-1]
 
     def register_reading(self,x,y,area):
-	self.mean,self.cov = self.kf.filter_update(self.mean,self.cov,[x,y,area])
+        if x == -1:
+            x = ma.masked
+        self.mean,self.cov = self.kf.filter_update(self.mean,self.cov,[x,y,area])
+
     def update(self):
-	x = vision.process_frame()
-	self.register_reading(x,0,0)
+        x = vision.process_frame()
+        self.register_reading(x,0,0)
         self.send_update()
+
     def x(self):
-	return self.mean[0]
+        return self.mean[0]
+
     def y(self):
-	return self.mean[1]
+        return self.mean[1]
+
     def area(self):
-	return self.mean[2]
+        return self.mean[2]
+
     def __repr__(self):
-	return str(self.x()) + ", " + str(self.y()) + ", " + str(self.area())
+        return str(self.x()) + ", " + str(self.y()) + ", " + str(self.area())
+
     def send_update(self):
-	print self
+        print self
         mpstate.master().track(self.x(),self.y(),self.area())
 
 def init(_mpstate):
@@ -46,7 +55,7 @@ def init(_mpstate):
 
 def vision_loop():
     while True:
-	time.sleep(.1)
+        time.sleep(.1)
         mpstate.tracker.update()
 
 def mavlink_packet(m):
