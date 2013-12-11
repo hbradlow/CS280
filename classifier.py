1f1a3f367ed87dd6e8d4d9d2f4dc02ff8d9bc506
Jonathan Ho <hojonathanho@gmail.com>
2013-12-11 15:23:00 -0800

    cross val + grid search

--- classifier.py
+++ classifier.py
@@ -1,6 +1,6 @@
 from generate_textons import generate_textons
 from feature_extraction import FeatureExtractor
-from sklearn import svm
+from sklearn import svm, cross_validation, grid_search
 
 class Classifier:
 
@@ -21,9 +21,17 @@ class Classifier:
             labels.append(label)
             feature_vectors.append(feature_vector)
 
+        X_train, X_test, y_train, y_test = cross_validation.train_test_split(feature_vectors, labels, test_size=.2, random_state=0)
+
         # train the classifier on the training data
-        self.classifier = svm.SVC()
-        self.classifier.fit(feature_vectors,labels)
+        svc = svm.SVC()
+        params = {'kernel':('linear',), 'C':[1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3]}
+        self.classifier = grid_search.GridSearchCV(svc, params)
+        self.classifier.fit(X_train, y_train)
+        print 'Best params:', self.classifier.best_params_
+
+        print 'Training data size:', len(X_train)
+        print 'Result on test set:', self.classifier.score(X_test, y_test)
 
     def predict(self,patch):
         """Predict the label of a patch using the trained classifier"""
