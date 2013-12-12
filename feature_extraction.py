20c06b41dc6ac52d07e3d84ce81c39e4181ff039
Henry Bradlow <hbradlow@berkeley.edu>
2013-12-11 16:46:41 -0800

    fixed color hists

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,12 +4,12 @@ import numpy as np
 class FeatureExtractor:
     def __init__(self,textons,bins=3):
         self.textons = textons
-        self.bins = 3
+        self.bins = 10
     def get_feature_vector(self,patch):
         gray_patch = np.mean(patch,axis=2)
         linear_patch = patch.reshape((-1,3))
 
         texton_hist = compute_texton_hist(self.textons,gray_patch)
-        color_hist = np.histogramdd(linear_patch,bins=self.bins)[0].reshape(-1)
+        color_hist = np.histogramdd(linear_patch,bins=self.bins,range=((-.5,255.5),(-.5,255.5),(-.5,255.5)))[0].reshape(-1)
 
         return np.r_[texton_hist, color_hist]
